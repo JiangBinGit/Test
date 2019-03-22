3a8ee9118a9ae03d0b4988339bce345eafaf77fb
jiangbin <jiangbin@aerozhonghuan.com>
2019-03-22 22:36:43 +0800

    增加日志

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -1,12 +1,17 @@
 package com.example.demo.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @RequestMapping("/testSpring")
     private String test(){
+        logger.info("=======");
         return "hello world";
     }
 }
