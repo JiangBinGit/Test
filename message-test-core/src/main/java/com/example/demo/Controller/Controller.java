d757d7431cd14fc9b339e2ced6a7695e0c3ae0bb
jiangbin <jiangbin@aerozhonghuan.com>
2019-03-30 23:01:26 +0800

    代码提交

--- message-test-core/src/main/java/com/example/demo/Controller/Controller.java
+++ message-test-core/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ public class Controller {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @RequestMapping("/testSpring")
-    private String test(){
+    protected String test(){
         logger.info("=======");
         logger.info("测试分支");
         return "hello world";
